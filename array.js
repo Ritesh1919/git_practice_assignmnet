cf1de3bb9e29c37fb78efaf1935925891cf9ed0f
ritesh <riteshkashyap9999@gmail.com>
2023-04-23 20:46:11 +0530

    Help me out

---
+++ array.js
@@ -0,0 +1,11 @@
+//  I am unable to find the count of all prime numbers presnent inside the array
+
+let arr=[];
+
+for(let i=0;i<arr.length;i++)
+{
+    if(num%i==0) 
+    {
+        count++;
+    }
+}
